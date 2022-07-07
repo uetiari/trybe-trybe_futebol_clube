95e7713a40105c889555570ca996f70c8c1f9997
Ariane Ueti <arianeueti@gmail.com>
2022-07-07 17:23:06 -0300

    Ajustes nas configs do Dockerfiles

--- app/backend/src/database/models/UsersModel.ts
+++ app/backend/src/database/models/UsersModel.ts
@@ -1,40 +1,40 @@
-// import { Model, DataTypes } from 'sequelize';
-// import db from '.';
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
 
-// export default class Users extends Model {
-//   public id: number;
-//   public username: string;
-//   public role: string;
-//   public email: string;
-//   public password: string;
-// }
+export default class Users extends Model {
+  public id: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
+}
 
-// Users.init({
-//   id: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//     autoIncrement: true,
-//     primaryKey: true,
-//   },
-//   username: { 
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   role: { 
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   email: { 
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   password: { 
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   }
-// }, {
-//   underscored: true,
-//   sequelize: db,
-//   modelName: 'users',
-//   timestamps: false,
-// });
+Users.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: { 
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: { 
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: { 
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: { 
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
